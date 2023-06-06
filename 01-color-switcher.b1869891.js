42ecbe41296d7ba32546c9367e951b859b009028
aaannetty <117479786+aaannetty@users.noreply.github.com>
2023-06-06 19:41:13 +0000

    Deploying to gh-pages from @ aaannetty/goit-js-hw-09@78c6d599332e69a603efb8fb602d062554319685 🚀

---
+++ 01-color-switcher.b1869891.js
@@ -0,0 +1,2 @@
+let t;document.querySelector("[data-start]").addEventListener("click",(function(){document.querySelector("[data-start]").disabled=!0,document.querySelector("[data-stop]").disabled=!1,t=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`;document.body.style.backgroundColor=t}),1e3)})),document.querySelector("[data-stop]").addEventListener("click",(function(){document.querySelector("[data-start]").disabled=!1,document.querySelector("[data-stop]").disabled=!0,clearInterval(t)}));
+//# sourceMappingURL=01-color-switcher.b1869891.js.map
